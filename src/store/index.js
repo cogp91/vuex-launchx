feb35e86a79c098db2a1990173a49ee386758ffc
Carlos Galvez <omargalvezp8@gmail.com>
2022-04-02 01:14:16 -0600

    random counter and random square counter files with vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,36 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    contador: 0
   },
   getters: {
+    cuadrado(state){
+      return state.contador * state.contador;
+    }
   },
-  mutations: {
+  mutations: { // we use commit for mutations (store.commit(''))
+    subirContador(state, random){
+      state.contador += random;
+    },
+    bajarContador(state, random){
+      state.contador -= random;
+    }
   },
-  actions: {
+  actions: { // we use dispatch for actions (store.dispatch('')) 
+    // we can use async in actions but not in mutations
+    async subirContador({commit}){
+      const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=8&col=1&base=10&format=plain&rnd=new');
+      const results = await res.json();
+      console.log(results);
+      commit('subirContador', results)
+    },
+
+    async bajarContador({commit}){
+      const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=8&col=1&base=10&format=plain&rnd=new');
+      const results = await res.json();
+      console.log(results);
+      commit('bajarContador', results)
+    },
   },
   modules: {
   }
